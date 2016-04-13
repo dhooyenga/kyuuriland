03d4a6fc104d20ca53920fdf91ec8a3d2c41b7e7
Dennis Hooijenga <dennis@hooyenga.me>
2016-04-13 13:21:11 +0200

    roar features

--- serverstatus.js
+++ serverstatus.js
@@ -1,19 +1,27 @@
-/*
-        Server status stuff
-*/
-
 $.getJSON("https://mcapi.ca/query/5.255.85.210:26263/list", function(serverStatus)
+{
+	// Server online y/n?
+	if(serverStatus.Status == true)
 	{
-		if(serverStatus.Status == true)
-	  {
-	  	console.log("server is online")
-	  } else {
-	  	console.log("server is offline")
-	  };
-	  $('.online-title').append(" (" + serverStatus.Players.online + "/" + serverStatus.Players.max + ")");
-	  if(serverStatus.Players.list != false){
-	    for (var i = 0; i < serverStatus.Players.list.length; i++) {
-	   		$('.online-players').append("<li class='player' style='background-image:url(https://mcapi.ca/avatar/2d/"+serverStatus.Players.list[i]+")'>"+serverStatus.Players.list[i]+"</li>")
-		}
-	  }
-	});
+		console.log("server is online")
+		$(".status").append("<span class='online'>online</span>.")
+	} else {
+		console.log("server is offline")
+	};
+	// Player count + max
+	$('.online-title').append(" (" + serverStatus.Players.online + "/" + serverStatus.Players.max + ")");
+	// Display online players
+	if(serverStatus.Players.list != false){
+		for (var i = 0; i < serverStatus.Players.list.length; i++) {
+			$('.online-players').append(
+				"<li class='player' style='background-image:url(https://mcapi.ca/avatar/2d/"+
+				serverStatus.Players.list[i]+
+				")'>"+
+				serverStatus.Players.list[i]+
+				"</li>"
+			)
+	  	}
+	} else {
+	  	$('.online-players').append("<li class='grijs'>The server is empty :(</li>")
+	}
+});
